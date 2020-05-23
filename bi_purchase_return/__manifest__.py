c8dc37080c58fc3a656950ec4caa2bf1629588b8
Mihran Thalhath <mihranz7@gmail.com>
2020-05-23 10:50:20 +0530

    add purchase return module

---
+++ bi_purchase_return/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Purchase Return",
+
+    'summary': """
+        Purchase Return Workflow""",
+
+    'description': """
+        Create a purchase return seperately and create corresponding deliveries
+        and corresponding debit note.
+    """,
+
+    'author': "Bassam Infotech LLP",
+    'website': "http://www.bassaminfotech.com",
+
+    'category': 'Purchase',
+    'version': '12.0.0.1',
+
+    'depends': ['base', 'purchase', 'account'],
+
+    'images': [
+        'static/description/icon.png',
+    ],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/purchase_return.xml',
+        'data/ir_sequence_data.xml',
+    ],
+}
